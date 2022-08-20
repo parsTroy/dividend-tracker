20a61530af4f6fbacfdc42ebbdd19f87bf54c362
parsTroy <troyparsons@hotmail.com>
2022-08-19 20:08:06 -0400

    Correction

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <div className={style.container}>
       <div className={style.rights}>
-        <p>The data returned by TrackYourDividends is for information and educational purposes only. Before making investment decisions, you should consider risk, timeframe, investment objectives, diversification, and a company’s financial health. The information from our site is not a recommendation to buy or sell a security. All investors should consult a qualified professional before trading in any security. Stock and option trading involves risk and is not suitable for all investors.</p>
+        <p>The data returned by Investation is for information and educational purposes only. Before making investment decisions, you should consider risk, timeframe, investment objectives, diversification, and a company’s financial health. The information from our site is not a recommendation to buy or sell a security. All investors should consult a qualified professional before trading in any security. Stock and option trading involves risk and is not suitable for all investors.</p>
         <p>Investation. © 2022</p>
       </div>
     </div>
