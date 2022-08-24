62c02c90d972fa6cfc4331d80d0247fcaef3680c
parsTroy <troyparsons@hotmail.com>
2022-08-24 19:33:05 -0400

    Total Value kind of works

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { fetchData } from "../../data";
+import React, { useEffect, useState } from "react";
 import AnnualIncome from "./AnnualIncome/AnnualIncome";
 import AverageCost from "./AverageCost/AverageCost";
 import Change from "./Change/Change";
@@ -15,11 +14,39 @@ import YieldOnCost from "./YieldOnCost/YieldOnCost";
 
 const Dashboard = () => {
 
+  const url = 'https://financialmodelingprep.com/api/v3/quote-short/QYLD?apikey=0b92bda5d2f297a72d359be292be3991';
+  const url2 = 'https://financialmodelingprep.com/api/v3/historical-price-full/stock_dividend/QYLD?apikey=0b92bda5d2f297a72d359be292be3991';
+  // const url2 = 'https://6300f6439a1035c7f8fb8068.mockapi.io/api/div/stock-data';
+
+  const [price, setPrice] = useState([])
+
+  const fetchData = () => {
+    fetch(url) // URL TO USE
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        setPrice(data)
+      })
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  const totalPrice = (price[0] && price[0].price);
+  const shareTotal = 35;
+
+  const totalValue = (totalPrice*shareTotal).toFixed(2);
+
+  // --------------------------------------------------------------
+
+
   return (
     <div className={style.container}>
       <div className={style.headingContainer}>
-        <div className={style.portfolioValue}>{fetchData}</div>
-        <div className={style.yield}>Portfolio Yield: {`9.87%`}</div>
+        <div className={style.portfolioValue}>{totalValue}</div>
+        <div className={style.yield}>Portfolio Yield: {portfolioYield}</div>
         <div className={style.yieldOnCost}>Yield on Cost: {`11.98%`}</div>
         <div className={style.annualIncome}>Annual Income: {`$2,400.12`}</div>
         <div className={style.beta}>Portfolio Beta: {`0.72`}</div>
