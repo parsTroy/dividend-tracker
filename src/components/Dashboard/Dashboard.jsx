22f017b390dc1aedfe7f06f4e1ee685799abd8f7
parsTroy <troyparsons@hotmail.com>
2022-08-24 19:35:35 -0400

    change to dashboard

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -15,7 +15,7 @@ import YieldOnCost from "./YieldOnCost/YieldOnCost";
 const Dashboard = () => {
 
   const url = 'https://financialmodelingprep.com/api/v3/quote-short/QYLD?apikey=0b92bda5d2f297a72d359be292be3991';
-  const url2 = 'https://financialmodelingprep.com/api/v3/historical-price-full/stock_dividend/QYLD?apikey=0b92bda5d2f297a72d359be292be3991';
+  // const url2 = 'https://financialmodelingprep.com/api/v3/historical-price-full/stock_dividend/QYLD?apikey=0b92bda5d2f297a72d359be292be3991';
   // const url2 = 'https://6300f6439a1035c7f8fb8068.mockapi.io/api/div/stock-data';
 
   const [price, setPrice] = useState([])
@@ -46,7 +46,7 @@ const Dashboard = () => {
     <div className={style.container}>
       <div className={style.headingContainer}>
         <div className={style.portfolioValue}>{totalValue}</div>
-        <div className={style.yield}>Portfolio Yield: {portfolioYield}</div>
+        <div className={style.yield}>Portfolio Yield: {'9.87'}</div>
         <div className={style.yieldOnCost}>Yield on Cost: {`11.98%`}</div>
         <div className={style.annualIncome}>Annual Income: {`$2,400.12`}</div>
         <div className={style.beta}>Portfolio Beta: {`0.72`}</div>
